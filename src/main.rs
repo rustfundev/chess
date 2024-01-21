ff4337fe3601e25c80f2864ae416b6c4cc8c8367
Tiago Mello <me@rustfun.dev>
2024-01-21 19:13:29 +0100

    Add changes

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,27 @@ struct Game {
     board: Board,
 }
 
+struct Move(String);
+
+impl Move {
+    fn from(&self) -> (usize, usize) {
+        (1, 0)
+    }
+    fn to(&self) -> (usize, usize) {
+        (2, 0)
+    }
+}
+
 impl Game {
+    fn play(&mut self, mov: Move) -> () {
+        let rank_from = mov.from().0;
+        let file_from = mov.from().1;
+        let rank_to = mov.to().0;
+        let file_to = mov.to().1;
+        println!("{:#?}", self.board[rank_to][file_to]);
+        self.board[rank_to][file_to] = self.board[rank_from][file_from];
+        self.board[rank_from][file_from] = Square::Empty;
+    }
     fn initialize(&mut self) -> () {
         self.board = [
             [
@@ -119,5 +139,6 @@ fn main() {
         board: [[Square::Empty; 8]; 8],
     };
     game.initialize();
-    println!("{:#?}", game);
+    game.play(Move(String::from("h5")));
+    println!("{:#?}", game.board);
 }
